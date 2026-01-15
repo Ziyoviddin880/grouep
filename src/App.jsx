c1ee2693ae50a6d6c34e44e9623b78b2c58ba814
Ziyoviddin880 <ziyoviddinabdurashidov7@gmail.com>
2026-01-15 15:50:19 +0500

    h2 tag add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <div>
-        <h1>Jaloliddin Xojimurodov</h1>
+      <h1>Jaloliddin Xojimurodov</h1>
+
+      <h2>Xojimurodov Jaloliddin</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
